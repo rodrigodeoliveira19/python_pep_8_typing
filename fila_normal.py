8f167224290ab1638f69d52e65f3324d0eaa69e9
rodrigodeoliveira19 <rodrigo.oliveira.iupp@zup.com.br>
2022-05-31 11:28:25 -0300

    Injeção de dependências

--- fila_normal.py
+++ fila_normal.py
@@ -1,8 +1,11 @@
 from typing import Dict, Union, List
 
 from constantes import CODIGO_NORMAL
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 from fila_base import FilaBase
 
+Estatisticas = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 class FilaNormal(FilaBase):
 
@@ -14,16 +17,6 @@ class FilaNormal(FilaBase):
         self.clientes_atendidos.append(cliente_atual)
         return f'cliente atual: {cliente_atual} dirija-se ao caixa {caixa}'
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[str, List[str], int]] = {}
-        if flag != "detail":
-            estatistica[f'{agencia}-{dia}'] =  len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['qtd_clientes_atendidos'] = (
-                len(self.clientes_atendidos)
-            )
+    def estatistica(self, retorna_estatistica: Estatisticas) -> dict:
 
-        return estatistica
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fila_normal import FilaNormal
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 
 # fila_teste = FilaNormal()
 # fila_teste.atualiza_fila()
@@ -12,4 +14,5 @@ from fabrica_fila import FabricaFila
 
 fila_teste2 = FabricaFila.pega_fila("NORMAL")
 fila_teste2.atualiza_fila()
-print(fila_teste2.estatistica("27/05/2022", 1044, "detail"))
+print(fila_teste2.chama_cliente(10))
+print(fila_teste2.estatistica(EstatisticaDetalhada("27/05/2022", 1044)))
